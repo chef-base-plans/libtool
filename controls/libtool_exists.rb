0b2c2380a5f17d71a059748b51452c3aa4ae73a9
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-30 12:38:11 +0100

    Update documentation
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/libtool_exists.rb
+++ controls/libtool_exists.rb
@@ -3,11 +3,14 @@ title 'Tests to confirm libtool exists'
 plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'libtool')
 
+
 control 'core-plans-libtool-exists' do
   impact 1.0
   title 'Ensure libtool exists'
   desc '
-  Verify libtool by ensuring bin/libtool exists'
+  Verify libtool by ensuring libtool and libtoolize
+  (1) exist and
+  (2) are executable'
   
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
@@ -16,9 +19,14 @@ control 'core-plans-libtool-exists' do
     its('stderr') { should be_empty }
   end
 
-  command_relative_path = input('command_relative_path', value: 'bin/libtool')
-  command_full_path = File.join(plan_installation_directory.stdout.strip, "#{command_relative_path}")
-  describe file(command_full_path) do
-    it { should exist }
+  [
+    "libtool",
+    "libtoolize"
+  ].each do |binary_name|
+    command_full_path = File.join(plan_installation_directory.stdout.strip, "bin", binary_name)
+    describe file(command_full_path) do
+      it { should exist }
+      it { should be_executable }
+    end
   end
 end
